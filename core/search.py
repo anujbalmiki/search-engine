54a88e6e4757fae8e7c97ebbc2fd4100fc0a0457
Anuj Balmiki <anuj@segoma.com>
2026-01-19 13:20:31 +0530

    Implement indexed search using tokenizer-derived single token

--- core/search.py
+++ core/search.py
@@ -18,3 +18,18 @@ class NaiveSearch():
             if cleaned_keyword in cleaned_text:
                 result.append(key)
         return result
+
+
+class IndexedSearch():
+
+    def __init__(self, index):
+        self.index_obj = index
+        self.tokenize = tokenize
+
+    def search(self, keyword):
+        if not isinstance(keyword, str) or not keyword.split():
+            raise ValueError("Keyword must be a non-empty string")
+        result = []
+        cleaned_keyword = self.tokenize(keyword)
+        result = list(self.index_obj.search(cleaned_keyword[0]))
+        return result
